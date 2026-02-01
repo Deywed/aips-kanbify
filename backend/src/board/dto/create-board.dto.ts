4358460874a14973ef151fc587668bdc7aaeb68f
Aleksandar Gospavić <aleksandar.gospavic@elfak.rs>
2026-02-01 19:41:07 +0100

    fix(board-entity): add description to board entity

--- backend/src/board/dto/create-board.dto.ts
+++ backend/src/board/dto/create-board.dto.ts
@@ -1,8 +1,20 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateBoardDto {
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   @MaxLength(100)
   title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  description?: string;
 }
