966e0b24c3f999824f5cd85980ba1d9be40147da
Aleksandar Gospavić <aleksandar.gospavic@elfak.rs>
2026-02-08 01:50:55 +0100

    feat(be/users): add search functionality for users

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { BoardMember } from 'src/board-members/entity/board-members.entity';
 import { User } from './entity/user.entity';
 
 @Injectable()
@@ -10,4 +11,26 @@ export class UsersService {
     @InjectRepository(User)
     private readonly userRepo: Repository<User>,
   ) {}
+
+  searchUsers(query: string, excludeBoardId?: string, currentUserId?: string) {
+    return this.userRepo
+      .createQueryBuilder('user')
+      .where('(user.email ILIKE :query OR user.username ILIKE :query)', {
+        query: `%${query}%`,
+      })
+      .andWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select('bm.userId')
+          .from(BoardMember, 'bm')
+          .where('bm.boardId = :excludeBoardId')
+          .andWhere('bm.userId != :currentUserId')
+          .getQuery();
+        return 'user.id NOT IN ' + subQuery;
+      })
+      .setParameter('excludeBoardId', excludeBoardId)
+      .setParameter('currentUserId', currentUserId)
+      .limit(10)
+      .getMany();
+  }
 }
