15b5b3ef75ac8ec7b71e0fdca15089632defbd7e
Aleksandar Gospavić <aleksandar.gospavic@elfak.rs>
2026-02-08 16:32:05 +0100

    feat(notifications): implement notifications controller and service methods for basic managing notifications

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -6,9 +6,11 @@ import { Notification } from './entity/notifications.entity';
 import { NotificationsService } from './notifications.service';
 import { NotificationListener } from './listeners/notification.listener';
 import { NotificationsGateway } from './notifications.gateway';
+import { NotificationsController } from './notifications.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification])],
   providers: [NotificationsService, NotificationListener, NotificationsGateway],
+  controllers: [NotificationsController],
 })
 export class NotificationsModule {}
